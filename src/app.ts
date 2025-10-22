d5cd0ec233462647a5cd0846132ef543a739023e
MartinGbz <martin.grabarz@outlook.fr>
2025-10-22 18:17:43 -0300

    🔥 Remove user route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 
 import { router as incentivesRoutes } from './routes/incentives.route';
-import { router as userRoutes } from './routes/user.routes';
 
 const app: Application = express();
 
 app.use('/incentives', incentivesRoutes);
-app.use('/users', userRoutes);
 
 app.use('/', (req: Request, res: Response): void => {
-  res.json({ message: "Miley, what's good?" });
+  res.json({ message: 'Not Found' });
 });
 
 export default app;
