1bd49fc6f890566d0b69e76e1da0524e4206d9e8
MartinGbz <martin.grabarz@outlook.fr>
2025-10-29 20:07:51 -0300

    🔥 remove unused files

--- src/constants/abis/index.ts
+++ src/constants/abis/index.ts
@@ -1,7 +1,3 @@
-export * from './a-token';
 export * from './aave-incentive-data-providers';
 export * from './aave-pool-data-providers';
 export * from './chainlink-price-feed';
-export * from './gho';
-export * from './stk-token';
-export * from './ui-pool-data-provider';
