4cca3315d5acefb548fc4552f7db2dc901f9369c
MartinGbz <martin.grabarz@outlook.fr>
2025-10-29 16:00:50 -0300

    🔧 change base timestamp for 0

--- src/lib/utils/timestamp.ts
+++ src/lib/utils/timestamp.ts
@@ -1,4 +1,4 @@
-export const BASE_TIMESTAMP = 1735700400; // 01/01/25 00:00:00 UTC
+export const BASE_TIMESTAMP = 0; // 01/01/1970 00:00:00 UTC
 
 export const getCurrentTimestamp = (): number => {
   return Math.floor(Date.now() / 1000);
