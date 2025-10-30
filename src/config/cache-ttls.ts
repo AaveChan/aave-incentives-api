38d52773852741d4da837cdbf723345f132be333
MartinGbz <martin.grabarz@outlook.fr>
2025-10-29 22:47:08 -0300

    🔧 update cache

--- src/config/cache-ttls.ts
+++ src/config/cache-ttls.ts
@@ -1,5 +1,5 @@
 export const CACHE_TTLS = {
-  TOKEN_PRICE: 60 * 15, // 15 min
+  TOKEN_PRICE: 60 * 10, // 10 min
   TOTAL_SUPPLY: 60 * 15, // 15 min
-  UI_INCENTIVES: 60 * 30, // 30 min
+  UI_INCENTIVES: 60 * 60, // 1 hour
 } as const;
