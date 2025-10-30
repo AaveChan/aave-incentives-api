f41be639012c36f839e438ff403a0045f92ef78b
MartinGbz <martin.grabarz@outlook.fr>
2025-10-30 17:19:22 -0300

    ✨ add a new ping route

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 
 import { router as incentivesRoutes } from './routes/incentives.route';
+import { router as pingRoute } from './routes/ping.route';
 
 const app: Application = express();
 
 app.use(cors());
 
 app.use('/incentives', incentivesRoutes);
+app.use('/ping', pingRoute);
 
 app.use('/', (_req: Request, res: Response): void => {
   res.status(400).send({ error: 'Not Found' });
