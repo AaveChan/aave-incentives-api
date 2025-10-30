fe457eac68e0cba74a6e8c7faafdbdbf3e76d7c1
MartinGbz <martingbz@proton.me>
2025-10-30 20:02:59 -0300

    🐛 Fix Vercel deployment (#8)

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 
+import { createLogger } from './config/logger';
 import { router as incentivesRoutes } from './routes/incentives.route';
+import { router as pingRoute } from './routes/ping.route';
+
+const PORT: number = 5050;
+
+const logger = createLogger('Server');
 
 const app: Application = express();
 
 app.use(cors());
 
 app.use('/incentives', incentivesRoutes);
+app.use('/ping', pingRoute);
 
 app.use('/', (_req: Request, res: Response): void => {
   res.status(400).send({ error: 'Not Found' });
+});
+
+app.listen(PORT, (): void => {
+  logger.info(`Server is running on ${PORT}...`);
 });
 
 export default app;
